8ba134f7d5d33b8cf1ec4385cc7cd54a67b36624
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-26 10:54:28 +0530

    Add React 17 and React 18 rendering examples

--- Day01_Part2/app.js
+++ Day01_Part2/app.js
@@ -1,11 +1,16 @@
 const element = React.createElement("h1", {}, "Hello Coder Army");
 
+// ReactDOM.render(element,document.getElementById('root'));
+// element : 50 card pade hue hai
+
 // ReactDOM.render() is deprecated in React 18
 // React 18 uses createRoot for better performance and concurrent rendering
 // createRoot allows React to manage updates more efficiently
 
 // const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
-// Line 7 more details:->
+// Reactroot.render(element);
+
+// Line 10 & 11 more details:->
 const container = document.getElementById("root");
 const reactRoot = ReactDOM.createRoot(container);
 
