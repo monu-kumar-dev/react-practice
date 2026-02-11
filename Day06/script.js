69df79b85604b457aa0067cd0553eb542521130c
monu-kumar-dev <monukumar.software@gmail.com>
2026-02-11 14:20:10 +0530

    Refactor counter to use React useState hook with functional updates

--- Day06/script.js
+++ Day06/script.js
@@ -1,25 +1,72 @@
-import React from "react";
+// This code using js Dom manipulation
+
+// import React from "react";
+// import ReactDom from "react-dom/client";
+
+// let count = 0;
+
+// // Implement counter using JavaScript DOM manipulation
+// function updateUI() {
+//   document.querySelector("h1").innerText = `Count is: ${count}`;
+//   document.querySelector(".firstButton").innerText = `Increment ${count}`;
+//   document.querySelector(".secondButton").innerText = `Decrement ${count}`;
+// }
+
+// function Counter() {
+//   function incrementNumber() {
+//     count += 1;
+//     console.log("Count value is: ", count);
+//     updateUI();
+//   }
+
+//   function decrementNumber() {
+//     count -= 1;
+//     console.log("Count value is: ", count);
+//     updateUI();
+//   }
+
+//   return (
+//     <div className="first">
+//       <h1>count is: {count}</h1>
+//       <button className="firstButton" onClick={incrementNumber}>
+//         Increment {count}
+//       </button>
+//       <button className="secondButton" onClick={decrementNumber}>
+//         Decrement {count}
+//       </button>
+//     </div>
+//   );
+// }
+
+// const container = document.getElementById("root");
+// const root = ReactDom.createRoot(container);
+// root.render(<Counter />);
+
+// Implement using useState hook:---------->
+
+import React, { useState } from "react";
 import ReactDom from "react-dom/client";
 
-let count = 0;
-
-function updateUI() {
-  document.querySelector("h1").innerText = `Count is: ${count}`;
-  document.querySelector(".firstButton").innerText = `Increment ${count}`;
-  document.querySelector(".secondButton").innerText = `Decrement ${count}`;
-}
-
+// count = 2;
+// setCount = 1, phhir useState me 1
+// setCount = 2, phhir useState me 2
 function Counter() {
+  let [count, setCount] = useState(0);
   function incrementNumber() {
-    count += 1;
+    // count += 1; // State is immutable. so, You never directly modify it.(Wrong)
+
+    // Correct:-->
+    // setCount(count + 1);
+
+    // Even better:--->
+    setCount((prev) => prev + 1);
+
     console.log("Count value is: ", count);
-    updateUI();
   }
 
   function decrementNumber() {
-    count -= 1;
+    setCount((prev) => prev - 1);
     console.log("Count value is: ", count);
-    updateUI()
   }
 
   return (
