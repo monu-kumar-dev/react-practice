239fd53f9d56124ea1064f413f90224c1a7afc9b
monu-kumar-dev <monukumar.software@gmail.com>
2026-02-11 09:26:18 +0530

    feat: display product price in Card component

--- Day05/Myntra_App/src/component/Card.js
+++ Day05/Myntra_App/src/component/Card.js
@@ -1,12 +1,17 @@
 function Card(props) {
   return (
     <div className="card" style={{ border: "2px solid black", padding: "2px" }}>
-      <img src={props.img_link} height="200px" width="250px" />
-
+      <img
+        src={props.img_link}
+        alt={props.cloth}
+        height="200px"
+        width="250px"
+      />
       <div style={{ textAlign: "center" }}>
         <h2>{props.cloth}</h2>
-        <h1>{props.offer}</h1>
-        <h2>Shop Now</h2>
+        <p>{props.offer}</p>
+        <p>₹{props.rate}</p>
+        <p>Shop Now</p>
       </div>
     </div>
   );
