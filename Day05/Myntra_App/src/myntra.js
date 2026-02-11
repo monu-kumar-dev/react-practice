a7b37f787c5fc1070e48d0d4cccde031aa41edef
monu-kumar-dev <monukumar.software@gmail.com>
2026-02-11 09:22:52 +0530

    add price sort functionality using button click

--- Day05/Myntra_App/src/myntra.js
+++ Day05/Myntra_App/src/myntra.js
@@ -3,7 +3,10 @@ import ReactDom from "react-dom/client";
 import Card from "./component/Card";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
-import products from "./utils/dummy";
+// import products from "./utils/dummy";
+import { useState } from "react";
+import productsData from "./utils/dummy";
+import Price from "./component/Price";
 // Yaha mai sikh raha hu ki agr ek file me agr export zyada hai tb use import kaise krenge. Upar jo import hai wo export default ke liye hai.
 
 // Note ye kewal export ke liye not for export default:--->
@@ -12,11 +15,17 @@ import products from "./utils/dummy";
 // we can also change the name of function using as like:--->
 import { Greet as Goa, Meet as Moa } from "./utils/dummy";
 
-
 function App() {
+  const [products, setProducts] = useState(productsData);
+  
+  function sortLowToHigh() {
+    const sorted = [...products].sort((a, b) => Number(a.Rs) - Number(b.Rs));
+    setProducts(sorted);
+  }
   return (
-    // Header
     <>
+      {/* Button for price high */}
+      <Price sort={sortLowToHigh} />
       <Header />
       {/* // Body */}
       <div className="middle">
@@ -28,6 +37,7 @@ function App() {
             img_link={item.img_link}
             cloth={item.name}
             offer={item.offer}
+            rate={item.Rs}
           />
         ))}
       </div>
