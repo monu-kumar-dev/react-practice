052042eb4bda0040d5e38205f7cd5c1ec082914d
monu-kumar-dev <monukumar.software@gmail.com>
2026-02-18 00:26:25 +0530

    feat: demonstrate React.memo behavior with prop changes

--- Day07/src/main.js
+++ Day07/src/main.js
@@ -1,50 +1,30 @@
 import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom/client";
+import Colorful from "./Component/Colorful";
 
 function Main() {
-  const [color, setColor] = useState("black");
-  document.body.style.backgroundColor = color;
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h1>Background color changer</h1>
-      <div className="but">
-        <button
-          style={{ backgroundColor: "red" }}
-          onClick={() => setColor("red")}
-        >
-          Red
-        </button>
-        <button
-          style={{ backgroundColor: "blue" }}
-          onClick={() => setColor("blue")}
-        >
-          Blue
-        </button>
-        <button
-          style={{ backgroundColor: "orange" }}
-          onClick={() => setColor("orange")}
-        >
-          Orange
-        </button>
-        <button
-          style={{ backgroundColor: "green" }}
-          onClick={() => setColor("green")}
-        >
-          Green
-        </button>
-        <button
-          style={{ backgroundColor: "pink" }}
-          onClick={() => setColor("pink")}
-        >
-          Pink
-        </button>
-        <button
-          style={{ backgroundColor: "black" }}
-          onClick={() => setColor("black")}
-        >
-          Reset
-        </button>
-      </div>
+      <h1>Counter is: {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+
+      {/* Understanding React.memo kya kya dhyan rakhta hai:--> */}
+
+      {/* After React.memo not run, jb increment increase hogaa */}
+      {/* <Colorful></Colorful> */}
+
+      {/* After React.memo not run, jb increment increase hogaa, kyuki yaha default value hai change nhi ho raha. */}
+      {/* <Colorful name="Monu"></Colorful> */}
+
+      {/* After React.memo run, kyuki yaha value baar baar change ho raha hai */}
+      <Colorful name={count}></Colorful>
     </>
   );
 }
