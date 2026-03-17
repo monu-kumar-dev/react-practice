31baa644e06bbf0fdacd377fa9f4145873eb1f30
monu-kumar-dev <monukumar.software@gmail.com>
2026-03-17 20:24:22 +0530

    Implement React Router with nested routes and index route in Details component

--- Day12/first.js
+++ Day12/first.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Router, Route, Routes, Link } from "react-router";
 import Home from "./Home";
 import Contact from "./Contact";
 import Dashboard from "./Dashboard";
+import Details from "./Details";
+import Zero from "./Zero";
+import Hello from "./Hello";
+import Hii from "./Hii";
 
 function App() {
   return (
@@ -12,12 +16,19 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/Contact">Contact</Link>
         <Link to="/Dashboard">Dashboard</Link>
+        <Link to="/Details">Details</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/Contact" element={<Contact></Contact>}></Route>
         <Route path="/Dashboard" element={<Dashboard></Dashboard>}></Route>
+        {/* Details inside Zero */}
+        <Route path="/Details" element={<Details></Details>}>
+          <Route index element={<Zero></Zero>}></Route>
+          <Route path="Hello" element={<Hello></Hello>}></Route>
+          <Route path="Hii" element={<Hii></Hii>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
